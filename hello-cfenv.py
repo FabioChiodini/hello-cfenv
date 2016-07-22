490b8ca078de16e3c6f5610acc82decd33d9eeb5
Fabio Chiodini <FabioChiodini@users.noreply.github.com>
2016-07-22 11:49:55 +0200

    Update hello-cfenv.py

--- hello-cfenv.py
+++ hello-cfenv.py
@@ -1,7 +1,8 @@
 from __future__ import print_function
 import os
 import uuid
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import socket
 from cfenv import AppEnv
 
 #cfenv = require("cfenv")
@@ -9,6 +10,7 @@ from cfenv import AppEnv
 
 env = AppEnv()
 app = Flask(__name__)
+hostk1 = socket.gethostname()
 my_uuid = str(uuid.uuid1())
 BLUE = "#0099FF"
 GREEN = "#33CC33"
@@ -17,7 +19,7 @@ COLOR = GREEN
 
 @app.route('/')
 def hello():
-    return render_template("index.html",bgcolor=COLOR,guid=my_uuid,namek=env.name,portk=env.port,appk=env.app)
+    return render_template("index.html",bgcolor=COLOR,guid=my_uuid,namek=env.name,hostk=hostk1,portk=env.port,appk=env.app)
 
 if __name__ == "__main__":
     app.run(debug=False,host='0.0.0.0', port=env.port)
